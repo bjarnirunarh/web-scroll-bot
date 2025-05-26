857989ab8d5e947c4ce93596e3eecf204c8f6c02
Bjarni Rúnar Heimisson <bjarnirunarh@gmail.com>
2025-05-26 18:44:05 +0000

    initialize and finalize

---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.mbl.is/fasteignir/fasteign/1456981/', {
+      waitUntil: 'networkidle2',
+    });
+
+    console.log("Page loaded");
+
+    await page.evaluate(() => {
+      window.scrollBy(0, 500);
+    });
+
+    console.log("Scrolled");
+
+    await page.waitForTimeout(5000);
+    await browser.close();
+    console.log("Browser closed");
+  } catch (error) {
+    console.error("❌ Error occurred:", error);
+  }
+})();
