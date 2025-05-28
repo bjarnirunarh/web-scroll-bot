5cf6be1df94c9c645806c91baa1bed54470bfa26
Bjarni Rúnar Heimisson <bjarnirunarh@gmail.com>
2025-05-28 11:08:00 +0000

    new link to house

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
     });
 
     const page = await browser.newPage();
-    await page.goto('https://www.mbl.is/fasteignir/fasteign/1456981/', {
+    await page.goto('https://www.mbl.is/fasteignir/fasteign/1464528/', {
       waitUntil: 'networkidle2',
     });
 
