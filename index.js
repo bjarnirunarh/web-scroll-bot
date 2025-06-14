424b53aee2eaff838050c0ca0df7906526322122
Bjarni Rúnar Heimisson <bjarnirunarh@gmail.com>
2025-06-14 17:44:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
     });
 
     const page = await browser.newPage();
-    await page.goto('https://www.mbl.is/fasteignir/fasteign/1472518/', {
+    await page.goto('https://www.mbl.is/fasteignir/fasteign/1473843/', {
       waitUntil: 'networkidle2',
     });
 
